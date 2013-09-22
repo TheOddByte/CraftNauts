e16b0c2692616af3436936c064e4f409f5db4869
Vilsol <vilsol2000@gmail.com>
2013-09-22 23:39:48 +0100

    Adding player api.

--- CraftNauts/api/char.lua
+++ CraftNauts/api/char.lua
@@ -1,11 +1,11 @@
 --[[
-Store information about character.
-Calculate collision volumes.
-Check for abilities.
-Check for actions: Jumping, Crouching, etc.
-Saving character to file.
-Character movement.
-Reading other players.
+	Store information about character.
+	Calculate collision volumes.
+	Check for abilities.
+	Check for actions: Jumping, Crouching, etc.
+	Saving character to file.
+	Character movement.
+	Reading other players.
 
 	Example for a character:
 
---
+++ CraftNauts/api/player.lua
@@ -0,0 +1,5 @@
+--[[
+	Receives input from player
+	Connects to all other API's
+	Talks directly to network.
+--]]
