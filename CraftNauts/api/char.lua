d8e0f6c29b0feb6ec600dee72fe9c53dbf7949e6
Vilsol <vilsol2000@gmail.com>
2013-09-22 23:41:24 +0100

    More updates on players and characters

--- CraftNauts/api/char.lua
+++ CraftNauts/api/char.lua
@@ -3,10 +3,7 @@
 	Calculate collision volumes.
 	Check for abilities.
 	Check for actions: Jumping, Crouching, etc.
-	Saving character to file.
-	Character movement.
-	Reading other players.
-
+	
 	Example for a character:
 
 	local characters = {}
--- CraftNauts/api/player.lua
+++ CraftNauts/api/player.lua
@@ -2,4 +2,5 @@
 	Receives input from player
 	Connects to all other API's
 	Talks directly to network.
+	Calculates other players
 --]]
