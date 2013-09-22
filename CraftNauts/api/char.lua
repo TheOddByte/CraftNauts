13480331d7dab5bd45c42d2223212fb96eb2b169
Symmetryc <yuiopqwertyasdfglkjh@hotmail.com>
2013-09-22 17:24:09 -0400

    Update char.lua

--- CraftNauts/api/char.lua
+++ CraftNauts/api/char.lua
@@ -1,9 +1,9 @@
 --[[
-Store information about character.
-Calculate collision volumes.
-Check for abilities.
-Check for actions: Jumping, Crouching, etc.
-Saving character to file.
-Character movement.
-Reading other players.
-]]
+    Store information about character.
+    Calculate collision volumes.
+    Check for abilities.
+    Check for actions: Jumping, Crouching, etc.
+    Saving character to file.
+    Character movement.
+    Reading other players.
+--]]
