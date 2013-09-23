52628a82c6fa1971f7ed2a10d914e78d0c77a6e5
Vilsol <vilsol2000@gmail.com>
2013-09-23 20:41:13 +0100

    Fixed for standards

--- CraftNauts/main.lua
+++ CraftNauts/main.lua
@@ -1,11 +1,18 @@
 path = shell.dir()
 
+--[[
+  Loads all files as API's in the API folder
+  @return boolean
+--]]
 local function loadAPIs()
 	files = fs.list(path .. "/api/")
 	for id, name in pairs(files) do
-		os.loadAPI(path .. "/api/" .. name)
+		if not os.loadAPI(path .. "/api/" .. name) then 
+			return false 
+		end
 		print("Loading: " .. name)
 	end
+	return true
 end
 
 loadAPIs()
