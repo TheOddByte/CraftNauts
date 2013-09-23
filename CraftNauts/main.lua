b847baa179bc860490e91f587d4182897ab2bb22
Vilsol <vilsol2000@gmail.com>
2013-09-24 00:00:40 +0100

    Fixing Indentation

--- CraftNauts/main.lua
+++ CraftNauts/main.lua
@@ -107,21 +107,6 @@ function read( _mask, _history, _limit )
   return input
 end
 
---[[
-  Loads all files as API's in the API folder
-  @return boolean
---]]
-local function loadAPIs()
-	files = fs.list(path .. "/api/")
-	for id, name in pairs(files) do
-		if not os.loadAPI(path .. "/api/" .. name) then 
-			return false 
-		end
-		print("Loading: " .. name)
-	end
-	return true
-end
-
 loadAPIs()
 
 --[[
