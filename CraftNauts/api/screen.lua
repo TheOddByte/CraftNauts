4f961268eba9fd9a4f81093bd1cdbbb14da92abe
Symmetryc <yuiopqwertyasdfglkjh@hotmail.com>
2013-09-23 21:35:38 -0400

    Update screen.lua

--- CraftNauts/api/screen.lua
+++ CraftNauts/api/screen.lua
@@ -3,3 +3,80 @@
     Draw the screen.
     Communicate with Map API
 --]]
+--[[
+    Screen API (WIP), sorry for hacky metatables
+    @version 0.1, 23/9/2013, Sym
+    @author Symmetryc, Sym
+--]]
+
+local xsize, ysize = term.getSize()
+
+--[[
+    Variable that buffers all of the information on the screen.
+    The metatable makes it so that buffer[1][1].character doesn't error
+    even if you haven't defined buffer[1] as a table.
+    It also sets a default for everything on the screen (so that we
+    don't need a term hijack)
+--]]
+local buffer = setmetatable({}, {
+  __index = function(self, key)
+    self[key] = setmetatable({}, {
+      __index = function(self2, key2)
+        self2[key2] = {
+          backgroundColor = colors.white;
+          textColor = colors.black;
+          character = "";
+        }
+        return self2[key2]
+      end;
+    })
+    return self[key]
+  end;
+})
+
+--[[
+    Caches the information so that it can be compared to the buffer
+    during draw time.
+--]]
+local cache = setmetatable({}, getmetatable(buffer))
+
+--[[
+    Used to set pixels of the buffer.
+    Usage: screen.setPixel[<x coord>][<y coord>](pixelData)
+    @param x coord number x coordinate
+    @param y coord number y coordinate
+    @param pixelData table contains background color in
+    "backgroundColor" index, text color in "textColor" index,
+    and character in "character" index.
+--]]
+setPixel = setmetatable({}, {
+  __index = function(self, key)
+    self[key] = setmetatable({}, {
+      __index = function(self2, key2)
+        self2[key2] = function(pixelData)
+          buffer[key][key2] = pixelData
+        end
+        return self2[key2]
+      end;
+    })
+    return self[key]
+  end;
+})
+
+--[[
+    Used to draw out the buffer to the computer screen, will
+    also cache the buffer for next comparison.
+--]]
+function drawBuffer()
+  for x = 1, xsize do
+    for y = 1, ysize do
+      if buffer[x][y] ~= cache[x][y] then
+        term.setCursorPos(x, y)
+        term.setTextColor(buffer[x][y].textColor)
+        term.setBackgroundColor(buffer[x][y].backgroundColor)
+        term.write(buffer[x][y].character)
+        cache[x][y] = buffer[x][y]
+      end
+    end
+  end
+end
